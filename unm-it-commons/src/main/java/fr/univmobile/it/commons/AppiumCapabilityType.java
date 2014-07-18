61c53582fb1d8756e998316052a429f9cd71b5e8
David <david.andriana@avantage-compris.com>
2014-07-18 18:16:06 +0200

    Add new project: unm-it-commons

---
+++ unm-it-commons/src/main/java/fr/univmobile/it/commons/AppiumCapabilityType.java
@@ -0,0 +1,10 @@
+package fr.univmobile.it.commons;
+
+public interface AppiumCapabilityType {
+
+	String APP = "app";
+	String DEVICE = "device";
+	String DEVICE_NAME = "deviceName";
+	String PLATFORM_NAME = "platformName";
+	String PLATFORM_VERSION = "platformVersion";
+}
