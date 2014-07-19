337e602c9e917bf1b76d191afc5f862892b4e087
David <david.andriana@avantage-compris.com>
2014-07-19 22:07:31 +0200

    Move commons classes from unm-ios-it to unm-it-commons

---
+++ unm-it-commons/src/main/java/fr/univmobile/it/commons/AppiumEnabledTestEngine.java
@@ -0,0 +1,39 @@
+package fr.univmobile.it.commons;
+
+import io.appium.java_client.AppiumDriver;
+
+import java.io.IOException;
+
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.remote.RemoteWebElement;
+
+interface AppiumEnabledTestEngine {
+
+	@Before
+	void setUp() throws Exception;
+
+	@After
+	void tearDown() throws Exception;
+
+	void takeScreenshot(String filename) throws IOException;
+
+	void savePageSource(String filename) throws IOException;
+
+	void swipe(int startX, int startY, int endX, int endY, int durationMs)
+			throws IOException;
+
+	RemoteWebElement findElementById(String id) throws IOException;
+
+	RemoteWebElement findElementByName(String name) throws IOException;
+
+	ElementChecker elementById(String id) throws IOException;
+
+	ElementChecker elementByName(String name) throws IOException;
+
+	AppiumDriver getDriver();
+
+	void pause(int ms) throws InterruptedException;
+
+	void futureScreenshot(int ms, String filename) throws IOException;
+}
---
+++ unm-it-commons/src/main/java/fr/univmobile/it/commons/ElementChecker.java
@@ -0,0 +1,14 @@
+package fr.univmobile.it.commons;
+
+import java.io.IOException;
+
+public interface ElementChecker {
+
+	void textShouldEqualTo(String ref) throws IOException;
+
+	void shouldBeVisible() throws IOException;
+
+	void shouldBeHidden() throws IOException;
+
+	void click() throws IOException;
+}
